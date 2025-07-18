8424c8549a47ff039ebe86b00754d7b917dc354f
NFTVault Developer <nftvault@example.com>
2025-07-18 01:34:49 +0000

    Fix mascot display issue - public folder was gitignored
    The mascot wasn't showing because:
    - The public folder is in .gitignore
    - Images in public/ weren't being deployed
    - Next.js only serves static files from public/
    
    Fixed by:
    - Adding exception for public/mascot.png in .gitignore
    - Using /mascot.png path (not /images/mascot.png)
    - Adding public/mascot.png to git tracking

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ function HomeContent() {
         {/* Mascot Image - Absolutely positioned */}
         <div className="absolute left-[250px] top-1/2 -translate-y-1/2">
           <img 
-            src="/images/mascot.png?v=1.4.3" 
+            src="/mascot.png?v=1.4.3" 
             alt="smol.markets mascot" 
             style={{ width: '192px', height: '192px' }}
             className="rounded-full object-cover"
@@ -62,7 +62,7 @@ function HomeContent() {
       {/* Mascot Image - Absolutely positioned */}
       <div className="fixed left-8 top-1/2 -translate-y-1/2 z-10">
         <img 
-          src="/images/mascot.png?v=1.4.3" 
+          src="/mascot.png?v=1.4.3" 
           alt="smol.markets mascot" 
           style={{ width: '384px', height: '384px' }}
           className="rounded-full opacity-90 object-cover"
